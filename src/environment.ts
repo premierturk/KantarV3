d52aac7f2454db98f4e47527c18b2baa62fdc19c
Deniz Arda Murat <amurat@pt.local>
2024-01-16 17:37:33 +0300

    v1.0.3 published

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  production: false,   // npm run electron serve de false olacak
-  apiUrl: 'http://10.210.210.95:2024/HYS.WebApi/api',
-  url:'http://10.210.210.95:2024'
+  production: true,   // npm run electron serve de false olacak
+  apiUrl: 'https://hybstest.izmir.bel.tr/HYS.WebApi/api',
+  url: 'https://hybstest.izmir.bel.tr'
+  //apiUrl: 'http://10.210.210.95:2024/HYS.WebApi/api',
+  //url: 'http://10.210.210.95:2024'
 };
